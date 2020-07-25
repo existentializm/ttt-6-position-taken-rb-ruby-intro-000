d850c3bcfdef0449cb30fffe67c75e109c6ee871
Liz Jaluague <e.jaluague@gmail.com>
2020-07-25 22:28:42 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,10 +3,7 @@
 def position_taken? (board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
-  else 
-    false
-  elsif board[index] == "X" || board[index] == "O"
+  else board[index] == "X" || board[index] == "O"
     true
-  else false
   end
 end
